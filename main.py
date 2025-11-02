198536da501bedb4fc3f095b43a9d6e05f94a3bd
lakhojunaid <j.ahmedlakho@gmail.com>
2025-11-02 15:54:40 -0500

    Added Integer Validation before addition

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
-num1= int(input("Enter first number: "))
-num2= int(input("Enter second number: "))
+num1 = input("Enter first number: ")
+try:
+    num1 = int(num1)
+except ValueError:
+    print("Invalid input. Please enter an integer.")
+    exit()
 
-sum= num1 + num2
+num2 = input("Enter second number: ")
+try:
+    num2 = int(num2)
+except ValueError:
+    print("Invalid input. Please enter an integer.")
+    exit()
+
+sum = num1 + num2
 print(sum)
