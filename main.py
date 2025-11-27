d55609f150dbf1844190053da65b3ca7ee106f24
lakhojunaid <j.ahmedlakho@gmail.com>
2025-11-26 20:53:12 -0500

    Replaced earlier input-based implementation to interpreter-based implementation

--- main.py
+++ main.py
@@ -1,16 +1,2 @@
-num1 = input("Enter first number: ")
-try:
-    num1 = int(num1)
-except ValueError:
-    print("Invalid input. Please enter an integer.")
-    exit()
-
-num2 = input("Enter second number: ")
-try:
-    num2 = int(num2)
-except ValueError:
-    print("Invalid input. Please enter an integer.")
-    exit()
-
-sum = num1 + num2
-print(sum)
+expr = input("Enter an expression: ")
+print("You entered:", expr)
